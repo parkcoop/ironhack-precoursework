af4b24772587db2c23c314ff3d78a63e6a93239a
Parker Cooper <parkr.coopr@gmail.com>
2019-03-18 22:18:59 -0400

    obstacle collission added

--- marsRover/javascripts/app.js
+++ marsRover/javascripts/app.js
@@ -23,23 +23,53 @@ let rover = {
   }
 };
 
+let rover2 = {
+  direction: "N",
+  x: 0,
+  y: 1,
+  travelLog: [],
+  moveNumber: 0,
+  keepTrack: function() {
+    this.moveNumber++;
+    currentPosition =
+      " \n" +
+      this.moveNumber +
+      ". Position: [" +
+      this.x +
+      ", " +
+      this.y +
+      "]. Facing: " +
+      this.direction +
+      " ";
+    this.travelLog.push(currentPosition);
+    console.log("History log: " + this.travelLog);
+  }
+};
+
+let obstacles = [
+  {
+    name: "meteor",
+    x: 3,
+    y: 2
+  },
+  {
+    name: "crater",
+    x: 9,
+    y: 2
+  },
+  {
+    name: "Elon Musk",
+    x: 5,
+    y: 5
+  }
+];
+
 console.log(
   "Mars Rover.\nAvailable commands: turnLeft() [l], turnRight() [r], moveForward() [f], moveBackward() [b].\nAvailable rovers: rover."
 );
 console.log('To execute multiple commands: controller("string of commands")');
-console.warn("Obstacles and collision not supported");
-const mars = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, "meteor", 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, "crater", 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
+console.warn("Multiple rovers not yet supported");
+
 // ======================
 function turnLeft(rover) {
   rover.keepTrack();
@@ -80,16 +110,18 @@ function turnRight(rover) {
 }
 
 function moveForward(rover) {
-  if (
+  if (checkObstacle(rover) == true) {
+    console.log("Please change direction and try again.");
+  } else if (
     (rover.x >= 10 && rover.direction == "E") ||
     (rover.y >= 10 && rover.direction == "S")
   ) {
-    console.log("Please change direction and try again");
+    console.log("Cliff! Please change direction and try again.");
   } else if (
     (rover.x <= 0 && rover.direction == "W") ||
     (rover.y <= 0 && rover.direction == "N")
   ) {
-    console.log("Please change direction and try again");
+    console.log("Cliff! Please change direction and try again.");
   } else {
     rover.keepTrack();
     switch (rover.direction) {
@@ -122,12 +154,12 @@ function moveBackward(rover) {
     (rover.x >= 10 && rover.direction == "W") ||
     (rover.y >= 10 && rover.direction == "N")
   ) {
-    console.log("Please change direction and try again");
+    console.log("Cliff! Please change direction and try again");
   } else if (
     (rover.x <= 0 && rover.direction == "E") ||
     (rover.y <= 0 && rover.direction == "S")
   ) {
-    console.log("Please change direction and try again");
+    console.log("Cliff! Please change direction and try again");
   } else {
     rover.keepTrack();
     switch (rover.direction) {
@@ -155,7 +187,7 @@ function moveBackward(rover) {
   }
 }
 
-function controller(commandString) {
+function controller(rover, commandString) {
   for (let i = 0; i < commandString.length; i++) {
     let character = commandString[i];
     switch (character) {
@@ -176,3 +208,70 @@ function controller(commandString) {
     }
   }
 }
+
+function checkObstacle(rover) {
+  for (let i = 0; i < obstacles.length; i++) {
+    switch (rover.direction) {
+      case "N":
+        if (rover.y - 1 == obstacles[i].y && rover.x == obstacles[i].x) {
+          console.log(
+            "Watch out! There's a " +
+              obstacles[i].name +
+              " located at (" +
+              obstacles[i].x +
+              ", " +
+              obstacles[i].y +
+              ")"
+          );
+
+          return true;
+        }
+        break;
+      case "E":
+        if (rover.x + 1 == obstacles[i].x && rover.y == obstacles[i].y) {
+          console.log(
+            "Watch out! There's a " +
+              obstacles[i].name +
+              " located at (" +
+              obstacles[i].x +
+              ", " +
+              obstacles[i].y +
+              ")"
+          );
+          return true;
+        }
+        break;
+      case "S":
+        if (rover.y + 1 == obstacles[i].y && rover.x == obstacles[i].x) {
+          console.log(
+            "Watch out! There's a " +
+              obstacles[i].name +
+              " located at (" +
+              obstacles[i].x +
+              ", " +
+              obstacles[i].y +
+              ")"
+          );
+          return true;
+        }
+        break;
+      case "W":
+        if (rover.x - 1 == obstacles[i].x && rover.y == obstacles[i].y) {
+          console.log(
+            "Watch out! There's a " +
+              obstacles[i].name +
+              " located at (" +
+              obstacles[i].x +
+              ", " +
+              obstacles[i].y +
+              ")"
+          );
+          return true;
+        }
+        return false;
+        break;
+    }
+  }
+}
+
+//Could probably be less repetitive
